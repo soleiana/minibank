eb26f29396dec4c1abb21b836d78e0d834ebd920
anna.su <anna2111@gmai.com>
2014-09-07 14:09:01 +0300

    project prototype

---
+++ src/main/java/com/minibank/core/events/loans/LoanExtensionDetails.java
@@ -0,0 +1,7 @@
+package com.minibank.core.events.loans;
+
+/**
+ * Created by Ann on 07/09/14.
+ */
+public class LoanExtensionDetails {
+}
---
+++ src/main/java/com/minibank/core/services/LoanEventHandler.java
@@ -0,0 +1,8 @@
+package com.minibank.core.services;
+
+/**
+ * Created by Ann on 07/09/14.
+ */
+public class LoanEventHandler implements LoanService
+{
+}
