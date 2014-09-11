b8cac2dd1408bd6636979759bae0b657f82c8d2d
anna.su <anna2111@gmai.com>
2014-09-11 19:34:11 +0300

    Hateoas, boot spring dependencies added

--- src/main/java/com/minibank/config/CoreConfig.java
+++ src/main/java/com/minibank/config/CoreConfig.java
@@ -4,6 +4,7 @@ package com.minibank.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
+
 /**
  * Created by Ann on 06/09/14.
  */
---
+++ src/main/java/com/minibank/hello/Application.java
@@ -0,0 +1,19 @@
+package com.minibank.hello;
+
+import com.minibank.config.DatasourceConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+@ComponentScan
+//@Import({DatasourceConfig.class})
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
