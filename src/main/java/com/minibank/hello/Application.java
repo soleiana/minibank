9f36fa1f91360c92fffd8403ff17c8f8781d8d56
anna.su <anna2111@gmai.com>
2014-09-12 11:33:59 +0300

    minor changes

--- src/main/java/com/minibank/hello/Application.java
+++ src/main/java/com/minibank/hello/Application.java
@@ -1,15 +1,18 @@
 package com.minibank.hello;
 
 import com.minibank.config.DatasourceConfig;
+import com.minibank.core.repository.LoanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @ComponentScan
-//@Import({DatasourceConfig.class})
+@Import({DatasourceConfig.class})
 @EnableAutoConfiguration
 public class Application {
+
 
     public static void main(String[] args)
     {
