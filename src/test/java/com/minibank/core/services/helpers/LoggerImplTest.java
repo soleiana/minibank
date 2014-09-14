bb626031d26c4559fb805f2dc6a8c0b0cee08486
anna.su <anna2111@gmai.com>
2014-09-14 09:16:42 +0300

    minor changes

---
+++ src/test/java/com/minibank/core/services/helpers/LoggerImplTest.java
@@ -0,0 +1,24 @@
+package com.minibank.core.services.helpers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Ann on 13/09/14.
+ */
+public class LoggerImplTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testLog() throws Exception {
+
+    }
+
+    @Test
+    public void testLog1() throws Exception {
+
+    }
+}
