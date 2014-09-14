e21a131fbdfa008d307f7a280a1887784d2a1aa1
anna.su <anna2111@gmai.com>
2014-09-14 17:39:58 +0300

    AllLoansEvent, RequestAllLoansEvent

--- src/main/java/com/minibank/core/events/loans/AllLoansEvent.java
+++ src/main/java/com/minibank/core/events/loans/AllLoansEvent.java
@@ -1,9 +1,22 @@
 package com.minibank.core.events.loans;
 
 import com.minibank.core.events.ReadEvent;
+import com.minibank.core.events.loans.domain.AllLoansDetails;
 
 /**
  * Created by Ann on 06/09/14.
  */
-public class AllLoansEvent extends ReadEvent {
+public class AllLoansEvent extends ReadEvent
+{
+    private AllLoansDetails allLoansDetails;
+
+    public AllLoansEvent(AllLoansDetails allLoansDetails)
+    {
+        this.allLoansDetails = allLoansDetails;
+    }
+
+    public AllLoansDetails getAllLoansDetails()
+    {
+        return allLoansDetails;
+    }
 }
--- src/main/java/com/minibank/core/events/loans/RequestAllLoansEvent.java
+++ src/main/java/com/minibank/core/events/loans/RequestAllLoansEvent.java
@@ -1,10 +1,22 @@
 package com.minibank.core.events.loans;
 
 import com.minibank.core.events.RequestReadEvent;
+import com.minibank.core.events.loans.domain.RequestAllLoansDetails;
 
 /**
  * Created by Ann on 06/09/14.
  */
 public class RequestAllLoansEvent extends RequestReadEvent
 {
+    private RequestAllLoansDetails requestAllLoansDetails;
+
+    public RequestAllLoansEvent(RequestAllLoansDetails requestAllLoansDetails)
+    {
+        this.requestAllLoansDetails = requestAllLoansDetails;
+    }
+
+    public RequestAllLoansDetails getRequestAllLoansDetails()
+    {
+        return requestAllLoansDetails;
+    }
 }
