65dd2571248726e624c9c168efda45598cadf902
anna.su <anna2111@gmai.com>
2014-09-15 16:47:07 +0300

    LoggerImplTest

--- src/test/java/com/minibank/core/services/helpers/LoggerImplTest.java
+++ src/test/java/com/minibank/core/services/helpers/LoggerImplTest.java
@@ -1,24 +1,85 @@
 package com.minibank.core.services.helpers;
 
+import com.minibank.SpringContextTest;
+import com.minibank.core.domain.*;
+import com.minibank.core.repository.CustomerRepository;
+import com.minibank.core.repository.DBException;
+import com.minibank.core.repository.LoanRequestRepository;
+import com.minibank.core.repository.RequestIPRepository;
+import com.minibank.core.repository.tools.DBCleaner;
+import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertNull;
+import static junit.framework.TestCase.assertTrue;
 
 /**
  * Created by Ann on 13/09/14.
  */
-public class LoggerImplTest {
+public class LoggerImplTest extends SpringContextTest
+{
+    @Autowired
+    private DBCleaner dbCleaner;
+    @Autowired
+    private RequestIPRepository requestIPRepository;
+    @Autowired
+    private LoanRequestRepository loanRequestRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private Logger logger;
+
+    private RequestIP requestIP;
+    private LoanRequest loanRequest;
+    private Customer customer;
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() throws DBException
+    {
+        dbCleaner.clear();
+        customer = CustomerFixture.standardCustomer();
+        customerRepository.create(customer);
+    }
 
+    private void createLoanRequest() throws DBException
+    {
+        loanRequest = LoanRequestFixture.standardLoanRequest();
+        requestIP = RequestIPFixture.standardRequestIP();
+        loanRequest.setRequestIP(requestIP);
+        loanRequest.setCustomer(customer);
     }
 
     @Test
-    public void testLog() throws Exception {
+    @Transactional
+    public void testLog_1() throws DBException
+    {
+        //scenario with new loan request with new ip address
 
+        createLoanRequest();
+        assertNull(loanRequest.getId());
+        assertNull(requestIP.getId());
+
+        logger.log(loanRequest);
+        assertNotNull(loanRequest.getId());
+        assertNotNull(requestIP.getId());
     }
 
     @Test
-    public void testLog1() throws Exception {
+    @Transactional
+    public void testLog_2() throws DBException
+    {
+        //scenario with new loan request coming from registered ip address
+        createLoanRequest();
+        requestIPRepository.create(requestIP);
+        assertNotNull(requestIP.getId());
 
+        logger.log(loanRequest);
+        assertNotNull(loanRequest.getId());
     }
+
 }
