a385e8deff30faae2bb20ccdb09065021ef727b8
anna.su <anna2111@gmai.com>
2014-09-16 12:47:13 +0300

    LoanExtensionController

--- src/main/java/com/minibank/rest/controller/LoanExtensionController.java
+++ src/main/java/com/minibank/rest/controller/LoanExtensionController.java
@@ -1,7 +1,35 @@
 package com.minibank.rest.controller;
 
+import com.minibank.core.events.loans.CreateLoanExtensionEvent;
+import com.minibank.core.events.loans.LoanExtensionCreatedEvent;
+import com.minibank.core.services.LoanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by Ann on 06/09/14.
  */
-public class LoanExtensionController {
+@Controller
+@RequestMapping("/rest/loanExtensions")
+public class LoanExtensionController
+{
+    @Autowired
+    private LoanService loanService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<String> createLoanExtension(@RequestBody String loanId)
+    {
+        Integer id = Integer.parseInt(loanId);
+        CreateLoanExtensionEvent createLoanExtensionEvent = new CreateLoanExtensionEvent(id);
+        LoanExtensionCreatedEvent loanExtensionCreatedEvent =
+                loanService.createLoanExtension(createLoanExtensionEvent);
+        String message = loanExtensionCreatedEvent.getMessage();
+
+        return new ResponseEntity<>(message, HttpStatus.CREATED);
+    }
 }
