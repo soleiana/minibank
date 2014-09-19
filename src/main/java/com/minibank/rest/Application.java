429ffc847a3736a89c9ca29cadc58aac8febc5cf
anna.su <anna2111@gmai.com>
2014-09-19 10:24:16 +0300

    Application.java: @Configuration added

--- src/main/java/com/minibank/rest/Application.java
+++ src/main/java/com/minibank/rest/Application.java
@@ -4,8 +4,10 @@ import com.minibank.config.DatasourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+@Configuration
 @ComponentScan(basePackages = "com.minibank")
 @Import({DatasourceConfig.class})
 @EnableAutoConfiguration
