915fba9837a4628ab154feb7c392283d36c3abeb
anna.su <anna2111@gmai.com>
2014-10-02 16:30:23 +0300

    rest.factories: interfaces removed

--- src/main/java/com/minibank/rest/factories/AllLoansRestFactory.java
+++ src/main/java/com/minibank/rest/factories/AllLoansRestFactory.java
@@ -2,11 +2,71 @@ package com.minibank.rest.factories;
 
 import com.minibank.core.communications.loans.domain.AllLoansDetails;
 import com.minibank.rest.domain.AllLoans;
+import com.minibank.rest.domain.Loan;
+import com.minibank.rest.domain.LoanExtension;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by Ann on 14/09/14.
+ * Created by Ann on 16/09/14.
  */
-public interface AllLoansRestFactory
+@Component
+public class AllLoansRestFactory
 {
-    AllLoans getNewAllLoans(AllLoansDetails allLoansDetails);
+    private Loan convert(com.minibank.core.communications.loans.domain.Loan fromLoan)
+    {
+        Loan toLoan = new Loan();
+        toLoan.setId(fromLoan.getId());
+        toLoan.setCurrInterestRate(fromLoan.getCurrInterestRate());
+        toLoan.setCurrInterest(fromLoan.getCurrInterest());
+        toLoan.setAmount(fromLoan.getAmount());
+        toLoan.setStartDate(fromLoan.getStartDate());
+        toLoan.setEndDate(fromLoan.getEndDate());
+        return toLoan;
+    }
+
+    private LoanExtension convert(com.minibank.core.communications.loans.domain.LoanExtension
+                                          fromLoanExtension)
+    {
+        LoanExtension toLoanExtension = new LoanExtension();
+        toLoanExtension.setLoanId(fromLoanExtension.getLoanId());
+        toLoanExtension.setInterestRate(fromLoanExtension.getInterestRate());
+        toLoanExtension.setInterest(fromLoanExtension.getInterest());
+        toLoanExtension.setStartDate(fromLoanExtension.getStartDate());
+        toLoanExtension.setEndDate(fromLoanExtension.getEndDate());
+        toLoanExtension.setSubmissionDate(fromLoanExtension.getSubmissionDate());
+        return  toLoanExtension;
+    }
+
+
+    public  AllLoans getNewAllLoans(AllLoansDetails allLoansDetails)
+    {
+        AllLoans allLoans = new AllLoans();
+        allLoans.setCustomerId(allLoansDetails.getCustomerId());
+        allLoans.setName(allLoansDetails.getName());
+        allLoans.setSurname(allLoansDetails.getSurname());
+
+        List<Loan> toLoans = new ArrayList<>();
+        for(com.minibank.core.communications.loans.domain.Loan fromLoan: allLoansDetails.getLoans())
+        {
+            Loan toLoan = convert(fromLoan);
+            List<LoanExtension> toLoanExtensions = new ArrayList<>();
+
+            for(com.minibank.core.communications.loans.domain.LoanExtension fromLoanExtension:
+                    fromLoan.getLoanExtensions())
+            {
+
+                LoanExtension toLoanExtension = convert(fromLoanExtension);
+                toLoanExtensions.add(toLoanExtension);
+            }
+            toLoan.setLoanExtensions(toLoanExtensions);
+
+            toLoans.add(toLoan);
+        }
+        allLoans.setLoans(toLoans);
+
+        return  allLoans;
+    }
 }
