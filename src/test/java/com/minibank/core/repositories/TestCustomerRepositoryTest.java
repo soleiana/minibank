becb42a13e756bef78345993447879f61e348b7f
soleiana <anna2111@gmai.com>
2016-02-25 19:57:11 +0200

    Add test for TestCustomerRepository

---
+++ src/test/java/com/minibank/core/repositories/TestCustomerRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.minibank.core.repositories;
+
+import com.minibank.SpringContextTest;
+import com.minibank.core.model.Customer;
+import com.minibank.core.model.CustomerFixture;
+import com.minibank.core.repositories.tools.DBCleaner;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import static org.junit.Assert.*;
+
+public class TestCustomerRepositoryTest extends SpringContextTest {
+
+    @Autowired
+    private DBCleaner dbCleaner;
+
+    @Autowired
+    private TestCustomerRepository testCustomerRepository;
+
+    private Customer customer;
+
+    @Before
+    public void setUp() {
+        dbCleaner.clear();
+        customer = CustomerFixture.standardCustomer();
+    }
+
+    @Test
+    @Transactional
+    public void testCreate() {
+        testCustomerRepository.create(customer);
+        assertNotNull(customer.getId());
+    }
+}
