7ace6081a5bd882cab5107ad03225a944c3f2662
soleiana <anna2111@gmai.com>
2016-02-29 11:01:06 +0200

    Add restassured support

---
+++ src/test/java/com/minibank/rest/controllers/integration/CustomerControllerIntegrationTest.java
@@ -0,0 +1,17 @@
+package com.minibank.rest.controllers.integration;
+
+import com.minibank.SpringContextTest;
+import com.minibank.core.repositories.helpers.DatabaseCleaner;
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CustomerControllerIntegrationTest extends SpringContextTest {
+
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
+    @Before
+    public void setUp() {
+        databaseCleaner.clear();
+    }
+}
